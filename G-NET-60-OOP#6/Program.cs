cb342aa9096e7fd5e53587757b6631ec8181182d
Abdelrahman Elhalby <abdoelhalby60@gmail.com>
2026-06-29 01:13:19 +0300

    Question01

--- G-NET-60-OOP#6/Program.cs
+++ G-NET-60-OOP#6/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-     
+            #region Part01
+            #region Question01
+            //Q1: What is abstraction in OOP? How is it different from encapsulation?
+            //    Give a real - world example(not from the session)
+            //        that shows the difference between the two.
+            
+
+            // Abstraction:
+            // Abstraction means showing only the essential features of an object
+            // while hiding the complex implementation details.
+            // It focuses on "what the object does", not "how it does it".
+
+            // Encapsulation:
+            // Encapsulation means wrapping data and methods inside a class
+            // and protecting the data by controlling access using properties
+            // or access modifiers (private, public, etc.).
+            // It focuses on protecting the object's data.
+
+            // Difference:
+            // - Abstraction hides implementation details.
+            // - Encapsulation hides and protects data from direct access.
+
+            // Real-world example:
+            // ATM Machine
+            // Abstraction:
+            // When you withdraw money, you only use the buttons and screen.
+            // You don't need to know how the bank processes your request internally.
+
+            // Encapsulation:
+            // Your bank account balance is protected.
+            // You cannot change it directly; it can only be updated through
+            // valid operations like Deposit() or Withdraw().
+
+
+
+            #endregion
+            #endregion
+
         }
     }
 }
